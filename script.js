40158d4615fce08aeb675d88379cc7eae4576dc1
xurume33 <leticiadiogenes0323@gmail.com>
2026-08-14 16:19:05 -0300

    atualizado

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
-const inputUpload = document.getElementById('meu-upload')
+const inputUpload = document.getElementById("meu-upload");
 
-const url_imagem = ""
-const nome = ""
+var nome = "";
+var url_imagem = "";
 
 inputUpload.addEventListener('change', function(event) {
-
-    const arquivo = event.target.files[0]
+    
+    const arquivo = event.target.files[0];
 
     if (arquivo) {
-        url_imagem = arquivo.name
-        console.log(url_imagem)
-    } else{
-        console.log("Nenhum arquivo selecionado.")
+        url_imagem = arquivo.name;
+        console.log(url_imagem);
+    } else {
+        url_imagem = "";
+        console.log("Nenhum arquivo selecionado.");
     }
-})
+});
 
 const inputNome = document.getElementById("nome")
+
 const button = document.getElementById("btn")
 
 button.addEventListener("click", () => {
     nome = inputNome.value
+    console.log(nome)
 })
 
-async function criar() {
-    const novo = { "nome": nome, "url": url_imagem};
-
-    const resposta = await fetch('http://localhost:3000', {
-    method: 'POST',
-    headers: {
-        'Content-Type': "application/json"
-    },
-    body: JSON.stringify(novo)
-    });
-
-    const resultado = await resposta.json();
-    console.log('criado com sucesso:', resultado);
+//POSt
+async function criarUsuario() { 
+    
+    const novoUsuario = { "nome": nome, "url": url_imagem}; 
+    
+    const response = await fetch('http://localhost:3000/alunos', { 
+        method: 'POST', 
+        headers: { 
+            'Content-Type': 'aplication/json' 
+        }, 
+        body: JSON.stringify(novoUsuario) 
+    })
 }
